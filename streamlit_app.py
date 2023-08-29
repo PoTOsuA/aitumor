99ffd12d0ff20deff65c0ef3a9ab265adae855a1
PoTOsuA <143535087+PoTOsuA@users.noreply.github.com>
2023-08-29 23:49:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
+# Load the pre-trained model
+model = load_model('path_to_your_model.h5')  # Load your trained model here
+
+# Streamlit app title and description
+st.title("Brain Tumor Classification")
+st.write("Upload an MRI scan and we will classify whether it contains a brain tumor or not.")
+
+# Upload image through Streamlit
+uploaded_file = st.file_uploader("Choose an MRI scan...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    # Display the uploaded image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded MRI scan", use_column_width=True)
+
+    # Preprocess the image for model prediction
+    # Perform any necessary preprocessing (resizing, normalization, etc.) on the uploaded image
+
+    # Make prediction using the model
+    # prediction = model.predict(preprocessed_image)
+    # Determine the class (tumor or non-tumor) based on the prediction
+
+    # Display the prediction result
+    # st.write("Prediction:", prediction)
+    # st.write("Class:", "Tumor" if prediction > 0.5 else "Non-Tumor")
